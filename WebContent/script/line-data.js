aad8b230bdca444e94e2b2306335739f8ca57af9
Mike D Pilsbury <mike.pilsbury@gmail.com>
2010-06-26 09:19:45 +0100

    Separate data and rendering.

---
+++ WebContent/script/line-data.js
@@ -0,0 +1,22 @@
+var tubeLines = {
+  district: {
+    name: 'District',
+    stations: [
+      { name: 'Ealing Broadway',
+        tick: TICKSTYLE.BOTH
+      },
+      { name: 'Ealing Common',
+        tick: TICKSTYLE.RIGHT
+      },
+      { name: 'Acton Town',
+        tick: TICKSTYLE.RIGHT
+      },
+      { name: 'Chiswick Park',
+        tick: TICKSTYLE.RIGHT
+      },
+      { name: 'Turnham Green',
+        tick: TICKSTYLE.RIGHT
+      },
+    ]
+  }
+};
