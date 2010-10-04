c598950dc2b89d45237799a65b94b923713a68e7
Mike D Pilsbury <mike.pilsbury@gmail.com>
2010-10-04 17:47:32 +0100

    Add 'time left' bar, to indicate estimated time until next monitor refresh.

--- static/script/monitor.js
+++ static/script/monitor.js
@@ -1,9 +1,24 @@
 $(document).ready(function () {
+  var refreshTimerFullWidth = $('.refresh-timer').width();
+  var refreshPeriod;
+  var expectedRefreshTime;
+
   new SocketSubscription({monitor: true}, function(message) {
-    console.log('message : ' + message);
+    //console.log('message : ' + message);
     var statistics = JSON.parse(message);
     
     $('.websocket-connections').text(statistics.clients);
     $('.memory-usage').text(JSON.stringify(statistics.memoryUsage));
+    
+    refreshPeriod = statistics.refreshPeriod;
+    expectedRefreshTime = new Date().getTime() + statistics.refreshPeriod;
+    $('.refresh-timer').width(refreshTimerFullWidth);
   });
+  
+  setInterval(function() {
+    var timeLeft = expectedRefreshTime - new Date().getTime();
+    var fractionLeft = timeLeft / refreshPeriod;
+    var pixels = fractionLeft * refreshTimerFullWidth;
+    $('.refresh-timer').width(pixels);
+  }, 100);
 });
