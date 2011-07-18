bd18cdf5f41474dc273653fc351094876d04dfdb
Mike D Pilsbury <mike.pilsbury@gmail.com>
2011-07-18 22:16:15 +0100

    Better HTML and CSS for line flag-boxes.

--- app.js
+++ app.js
@@ -27,17 +27,17 @@ app.configure('production', function(){
 app.get('/', function(req, res){
   res.render('index', {
     lines: [
-      {name: 'Bakerloo', class: "bakerloo", code: 'B'},
-      {name: 'Central', class: "central", code: 'C'},
-      {name: 'Circle', class: "circle", code: '?'},
-      {name: 'District', class: "district", code: 'D'},
-      {name: 'Hammersmith & City', class: "hammersmith-city", code: 'H'},
-      {name: 'Jubilee', class: "jubilee", code: 'J'},
-      {name: 'Metropolitan', class: "metropolitan", code: 'M'},
-      {name: 'Northern', class: "northern", code: 'N'},
-      {name: 'Piccadilly', class: "piccadilly", code: 'P'},
-      {name: 'Victoria', class: "victoria", code: 'V'},
-      {name: 'Waterloo & City', class: "waterloo-city", code: 'W'}
+      {displayName: 'Bakerloo', name: "bakerloo", code: 'B'},
+      {displayName: 'Central', name: "central", code: 'C'},
+      {displayName: 'Circle', name: "circle", code: 'H'},
+      {displayName: 'District', name: "district", code: 'D'},
+      {displayName: 'Hammersmith & City', name: "hammersmith-city", code: 'H'},
+      {displayName: 'Jubilee', name: "jubilee", code: 'J'},
+      {displayName: 'Metropolitan', name: "metropolitan", code: 'M'},
+      {displayName: 'Northern', name: "northern", code: 'N'},
+      {displayName: 'Piccadilly', name: "piccadilly", code: 'P'},
+      {displayName: 'Victoria', name: "victoria", code: 'V'},
+      {displayName: 'Waterloo & City', name: "waterloo-city", code: 'W'}
     ]
   });
 });
