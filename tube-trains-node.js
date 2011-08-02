fb676ff7d8926dd5f044fc6810c6d3c1cdebbb72
Mike D Pilsbury <mike.pilsbury@gmail.com>
2011-08-02 16:53:32 +0100

    Remove another redundant js file.

--- tube-trains-node.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const staticserver = require('./lib/staticserver');
-const websocket = require('./lib/websocket');
-
-const port = 8003;
-const webroot = path.join(path.dirname(__filename), 'static');
-
-const server = staticserver.create(port, webroot);
-websocket.addToServer(server);
