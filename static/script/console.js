9992b7fd5e2001065e9c6fc69497ee299c015e14
Mike D Pilsbury <mike.pilsbury@gmail.com>
2011-08-02 15:26:27 +0100

    Remove old static files (from pre-express/jade use).

--- static/script/console.js
+++
@@ -1,9 +0,0 @@
-// Ensure that use of console.* functions does not break on platforms
-// that don't support them.
-
-if (!window.console) console = {};
-
-console.log = console.log || function(){};
-console.warn = console.warn || function(){};
-console.error = console.error || function(){};
-console.info = console.info || function(){};
